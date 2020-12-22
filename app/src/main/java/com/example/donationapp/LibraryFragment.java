276f66e1ae413ce7f4ca89ad4e506b0a179be9a1
Gokul Ramasamy <69314159+Gokul-Rams@users.noreply.github.com>
2020-12-22 12:13:21 +0530

    updated logout activities

--- app/src/main/java/com/example/donationapp/LibraryFragment.java
+++ app/src/main/java/com/example/donationapp/LibraryFragment.java
@@ -4,15 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+
 public class LibraryFragment extends Fragment {
+
+    TextView tvusername,tvusertype;
+    ImageView ivuserimage;
+    ExtendedFloatingActionButton btnlogout,btneditprofile;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_library,container,false);
+        View view = inflater.inflate(R.layout.fragment_library,container,false);
+        tvusername = view.findViewById(R.id.tvusername_library_fragment);
+        tvusertype = view.findViewById(R.id.tvusertype_library_fragment);
+        ivuserimage = view.findViewById(R.id.ivuserimage_library_fragment);
+        btnlogout = view.findViewById(R.id.btnlogout_library_fragment);
+        btneditprofile = view.findViewById(R.id.btnedit_profile_library_fragment);
+
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btneditprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/donationapp/entities/CurrentUser.java
@@ -0,0 +1,7 @@
+package com.example.donationapp.entities;
+
+public class CurrentUser {
+    String uid,name,phno,email;
+    Boolean isassociation;
+    String assname;
+}
